d6397d0889cf82f5eaa696162997f55254b34b42
matthewslima22 <matthewssilva96@gmail.com>
2021-03-20 17:33:27 -0300

    init

---
+++ gestaoderestauranteindra/backend/src/main/java/com/indra/gestaoindra/GestaoindraApplication.java
@@ -0,0 +1,13 @@
+package com.indra.gestaoindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoindraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoindraApplication.class, args);
+	}
+
+}
---
+++ gestaoderestauranteindra/backend/src/test/java/com/indra/gestaoindra/GestaoindraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indra.gestaoindra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoindraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
